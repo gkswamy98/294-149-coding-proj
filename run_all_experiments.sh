d78fd95116f8759cc4796d168d6e4f9a7a27fda5
Gokul Swamy <gokul.swamy@berkeley.edu>
2018-10-04 23:43:02 -0700

    allbig kahuna

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -2,5 +2,5 @@
 for ddpg in {1..10}
 do
    maddpg="$((10-$ddpg))"
-   python3 run.py --num-agents=10 --num-ddpg=$ddpg --num-maddpg=$maddpg --max-episode-len=10 --num-episodes=10000 --save-suffix="ddpg_$ddpg"
+   python3 run.py --num-agents=10 --num-ddpg=$ddpg --num-maddpg=$maddpg --max-episode-len=10 --num-episodes=5000 --save-suffix="ddpg_$ddpg"
 done
