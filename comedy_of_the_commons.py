56494fdbad6f073fe41dfe6defdd4b262f427bd3
Gokul Swamy <gokul.swamy@berkeley.edu>
2018-10-04 19:08:23 -0700

    whoops

--- comedy_of_the_commons.py
+++ comedy_of_the_commons.py
@@ -11,10 +11,10 @@ class COTCEnv(object):
 		info_n = [None] * len(action_n)
 		self.points += [(act_n.count(i) == 1) * i for i in act_n] # isn't this slick
 		if self.t % self.ep_len == 0: # Episode is over
-			rew_n = [0] * len(action_n)
+			rew_n = [np.where(np.argsort(self.points) == i)[0][0] + 1 for i in range(len(self.points))] # slicker than an electric eel
 			done_n = [True] * len(action_n)
 		else:
-			rew_n = [np.where(np.argsort(self.points) == i)[0][0] + 1 for i in range(len(self.points))] # slicker than an electric eel
+			rew_n = [0] * len(action_n)
 			done_n = [False] * len(action_n)
 		self.t += 1
 		return [np.copy(self.points) for _ in range(self.num_agents)], rew_n, done_n, info_n
